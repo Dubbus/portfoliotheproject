9480877018d65bbb81319c6b69760feeec68918d
Adarsh Muralidharan <165833439+Dubbus@users.noreply.github.com>
2025-03-12 01:41:24 -0400

    interfaces let's gooo sorry jeremy for the deadline heroing

---
+++ src/BookRecommenderKernelInterface.java
@@ -0,0 +1,32 @@
+/*book recommender kernel component with primary methods
+ * @initially <pre>
+ * (): ensures this = {}
+ * (Book b): ensures this = #this ∪ {b}
+ * (String title): ensures this = #this - {b in #this | b.title = title}
+ * </pre>
+ */
+
+import components.standard.Standard;
+
+
+/**
+ * book recommender kernel component with primary methods.
+ *
+ * @initially <pre>
+ *  ():
+ *   ensures this = {}
+ *  (book b):
+ *   ensures this = #this ∪ {b}
+ *  (string title):
+ *   ensures this = #this - {b in #this | b.title = title}
+ * </pre>
+ */
+public interface BookRecommenderKernelInterface extends Standard<BookRecommenderKernelInterface> {
+
+    /*adds a book to the recommender system
+     * @param b the book to be added
+     * @updates this
+     * @ensures this = #this + b
+     */
+    void addBook(Book b);
+}
