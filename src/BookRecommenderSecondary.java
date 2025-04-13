e6e65c599a7970fb10287f94e972b3f2a5b81862
Adarsh Muralidharan <165833439+Dubbus@users.noreply.github.com>
2025-04-12 20:22:07 -0400

    added one more enhance method

---
+++ src/BookRecommenderSecondary.java
@@ -0,0 +1,11 @@
+public abstract class BookRecommenderSecondary {
+
+    /**
+     * gets recommendations for a given book title.
+     * @param title the title of the book to base recommendations on
+     * @return a map containing recommended books keyed by their title
+     */
+    Map<String, Book> getRecommendations(String title){
+
+    }
+}
