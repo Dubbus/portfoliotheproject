74428fb550c918a70199ed4ddcd8a9e1004284e1
Adarsh Muralidharan <165833439+Dubbus@users.noreply.github.com>
2025-04-15 13:18:42 -0400

    initial abstract class

---
+++ src/BookRecord.java
@@ -0,0 +1,16 @@
+/**
+ * Represents a Book.
+ */
+public record BookRecord() {
+
+    /**
+     * Represents a tracker for a book, containing details such as title,
+     * author, genre, and the number of pages.
+     *
+     * @param title  the title of the book
+     * @param author the author of the book
+     * @param genre  the genre of the book
+     * @param pages  the number of pages in the book
+     */
+    public record BookTracker(String title, String author, String genre, int pages) { }
+}
